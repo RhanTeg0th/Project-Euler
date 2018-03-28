55b95e777f9aab822ff991e472102b2f5395d971
RhanTeg0th <Tong.Duurai@gmail.com>
2018-03-28 18:21:56 +0000

    Update problem1.c

--- problem1.c
+++ problem1.c
@@ -5,34 +5,37 @@
 /* Project Euler problem #1. Written by Anton L Feichtmeir using Emacs24/GCC. 
    Date: 3/17/18   */
 
+static unsigned long total;
+
 int main(void)
 {
 
 
+  total = 0;
 
-  unsigned long s3 = 0;
+ 
 
  
 
   for(unsigned int i = 1; i < 334; i++){
 
-     s3 = s3 + (3*i);
+     total  = total + (3*i);
 
     
   }
 
 
-  unsigned long s5 = 0;
+  
 
   for(unsigned int j = 1; j < 1000; j++){
 
     if(j%5==0 && j%3!=0){
 
-      s5 = s5 + j;
+      total  = total + j;
 
     }else{
 
-      s5 = s5 + 0;
+      total = total + 0;
 
     }
 
@@ -42,7 +45,7 @@ int main(void)
 
 
   printf("Project Euler problem 1 solution = ");
-  printf("%lu \n", s3+s5);
+  printf("%lu \n", total);
  
 
   return 0;
